21937d90c96d7c7798e693b1b81168aa7e64813e
zixcon <zixcon.fan@outlook.com>
2017-12-25 22:06:35 +0800

    a

--- crap/crap/spiders/girl.py
+++ crap/crap/spiders/girl.py
@@ -1,22 +1,31 @@
 # -*- coding: utf-8 -*-
 # https://docs.scrapy.org/en/latest/
 import scrapy
-
+import wget
 
 class GirlSpider(scrapy.Spider):
     name = "girl"
-    allowed_domains = ["www.xiaohuar.com"]
-    start_urls = ['http://www.xiaohuar.com/mm/']
+    allowed_domains = ['www.xiaohuar.com']
+    start_urls = ['http://www.xiaohuar.com/p/suyan/']
 
     def parse(self, response):
         current_url = response.url  # 爬取时请求的url
         body = response.body  # 返回的html
         unicode_body = response.body_as_unicode()  # 返回的html unicode编码
 
-        for item in response.css('div.items'):
+        # for item in response.css('div.items'):
+        #     yield {
+        #     'src':item.css('img::attr(src)').extract_first(),
+        #     'name':item.css('p > a::text').extract_first(),
+        #     }
+
+        for item in response.css('div.item'):
+            href = 'http://www.xiaohuar.com' + item.css('div.img a > img::attr(src)').extract_first()
+            name = item.css('div.img a > img::attr(alt)').extract_first()
+            wget.download(href,name+'.jpg')
             yield {
-            'src':item.css('img::attr(src)').extract_first(),
-            'name':item.css('p > a::text').extract_first(),
+            'href':'http://www.xiaohuar.com' + item.css('div.img a > img::attr(src)').extract_first(),
+            'name':item.css('div.img a > img::attr(alt)').extract_first(),
             }
 
 

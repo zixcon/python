bbb74f5a560eda3946612a5ab6a74537e5efbd58
zixcon <zixcon.fan@outlook.com>
2017-12-25 20:31:18 +0800

    url

--- crap/crap/spiders/girl.py
+++ crap/crap/spiders/girl.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
+# https://docs.scrapy.org/en/latest/
 import scrapy
 
 
 class GirlSpider(scrapy.Spider):
     name = "girl"
     allowed_domains = ["www.xiaohuar.com"]
-    start_urls = ['http://www.xiaohuar.com/']
+    start_urls = ['http://www.xiaohuar.com/mm/']
 
     def parse(self, response):
-        pass
+        current_url = response.url  # 爬取时请求的url
+        body = response.body  # 返回的html
+        unicode_body = response.body_as_unicode()  # 返回的html unicode编码
+
+        for item in response.css('div.items'):
+            yield {
+            'src':item.css('img::attr(src)').extract_first(),
+            'name':item.css('p > a::text').extract_first(),
+            }
+
+

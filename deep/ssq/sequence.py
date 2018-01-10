ba5bb970bfe629c9203a64172e2b9b991d8cea9c
zixcon <zixcon.fan@outlook.com>
2018-01-10 21:51:37 +0800

    deep

--- deep/ssq/sequence.py
+++ deep/ssq/sequence.py
@@ -6,16 +6,16 @@ import pandas as pd
 
 csv_reader = csv.reader(open('ball_items.csv', encoding='utf-8'))
 
-x_data, y_data=[],[]
+x_data, y_data = [], []
 for row in csv_reader:
     x_data.append(row[3])
-    y_data.append(row[6])
-x_data, y_data=x_data[1:],y_data[1:]
+    y_data.append(row[1])
+x_data, y_data = x_data[1:], y_data[1:]
 print(x_data)
 print(y_data)
 
-plt.plot(y_data, x_data, c='r')
-plt.show()
+# plt.plot(y_data, x_data, c='r')
+# plt.show()
 
-df = pd.DataFrame(x_data,index = y_data,columns=list('A'))
-df
+df = pd.DataFrame(x_data, index=y_data, columns=list('A'))
+df.plot()

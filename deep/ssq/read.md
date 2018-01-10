5832f62551c2a9ae753ae7fa7d061cd360903e0a
zixcon <zixcon.fan@outlook.com>
2018-01-10 21:07:09 +0800

    add piliang

---
+++ deep/ssq/read.md
@@ -0,0 +1,3 @@
+可视化
+#### http://usyiyi.cn/translate/Pandas_0j2/visualization.html
+#### https://amaozhao.gitbooks.io/pandas-notebook/content/%E6%97%B6%E9%97%B4%E5%BA%8F%E5%88%97%E7%BB%98%E5%9B%BE.html
--- deep/ssq/sequence.py
+++ deep/ssq/sequence.py
@@ -12,15 +12,10 @@ for row in csv_reader:
     y_data.append(row[6])
 x_data, y_data=x_data[1:],y_data[1:]
 print(x_data)
-print(y_data)  
+print(y_data)
 
 plt.plot(y_data, x_data, c='r')
 plt.show()
 
 df = pd.DataFrame(x_data,index = y_data,columns=list('A'))
 df
-#
-
-# http://usyiyi.cn/translate/Pandas_0j2/visualization.html
-# https://amaozhao.gitbooks.io/pandas-notebook/content/%E6%97%B6%E9%97%B4%E5%BA%8F%E5%88%97%E7%BB%98%E5%9B%BE.html
-

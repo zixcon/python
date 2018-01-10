083265f0d96188523fec372aa38b511de9ebeca6
zixcon <zixcon.fan@outlook.com>
2018-01-10 19:48:20 +0800

    Update ve.py

--- script/ve.py
+++ script/ve.py
@@ -20,3 +20,7 @@ plt.show()
 df = pd.DataFrame(x_data,index = y_data,columns=list('A'))
 df
 #
+
+# http://usyiyi.cn/translate/Pandas_0j2/visualization.html
+# https://amaozhao.gitbooks.io/pandas-notebook/content/%E6%97%B6%E9%97%B4%E5%BA%8F%E5%88%97%E7%BB%98%E5%9B%BE.html
+

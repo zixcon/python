8ed5f91f0e30d76036e6f05a6cee6f9b23176e92
zixcon <zixcon.fan@outlook.com>
2018-01-10 23:46:31 +0800

    visual

--- deep/ssq/sequence.py
+++ deep/ssq/sequence.py
@@ -7,30 +7,39 @@ import pandas as pd
 csv_reader = csv.reader(open('ball_items.csv', encoding='utf-8'))
 
 x_data, y_data = [], []
+i = 0
 for row in csv_reader:
-    x_data.append(row[3])
-    y_data.append(row[2])
-x_data, y_data = x_data[1:], y_data[1:]
-print(x_data)
-print(y_data)
+    if i != 0:
+        x_data.append(int(row[3]))
+        y_data.append(row[1])
+    i = i + 1
+# X, Y = x_data[1:], y_data[1:]
+X, Y = x_data, y_data
+print(X)
+print(Y)
 
 # plt.plot(y_data, x_data, c='r')
 # plt.show()
-#
-# ts = pd.Series(x_data, index=y_data)
-# ts.plot()
-# df = pd.DataFrame(x_data, index=y_data, columns=list('A'))
-# plt.figure()
-# df.plot()
 
-ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-ts = ts.cumsum()
+ts = pd.Series(X, index=Y)
+# ts = ts.cumsum()
 ts.plot()
 plt.show()
 
-df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
-df = df.cumsum()
+df = pd.DataFrame(X, index=Y, columns=list('A'))
+# df = df.cumsum()
 plt.figure()
 df.plot()
-# 增加后即可展示图片，否则pycharm不展示图片
 plt.show()
+
+# ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+# ts = ts.cumsum()
+# ts.plot()
+# plt.show()
+#
+# df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
+# df = df.cumsum()
+# plt.figure()
+# df.plot()
+# # 增加后即可展示图片，否则pycharm不展示图片
+# plt.show()
--- visual/pandas/series.py
+++ visual/pandas/series.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+x_data = np.random.randn(1000)
+print(x_data)
+y_data = pd.date_range('1/1/2000', periods=1000)
+print(y_data)
+
+ts = pd.Series(x_data, index=y_data)
 ts = ts.cumsum()
 ts.plot()
 plt.show()
 
-df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
+df = pd.DataFrame(np.random.randn(1000, 4), index=y_data, columns=list('ABCD'))
 df = df.cumsum()
 plt.figure()
 df.plot()

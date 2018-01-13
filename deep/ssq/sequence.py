4c99edc5c34b3a1ed364daa8a4ad554ca807c431
zixcon <zixcon.fan@outlook.com>
2018-01-13 13:42:00 +0800

    test

--- deep/ssq/sequence.py
+++ deep/ssq/sequence.py
@@ -1,6 +1,5 @@
-import numpy as np
 import csv
-import tensorflow as tf
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -11,7 +10,7 @@ i = 0
 for row in csv_reader:
     if i != 0:
         x_data.append(int(row[3]))
-        y_data.append(row[1])
+        y_data.append(row[0])
     i = i + 1
 # X, Y = x_data[1:], y_data[1:]
 X, Y = x_data, y_data
@@ -31,15 +30,3 @@ df = pd.DataFrame(X, index=Y, columns=list('A'))
 plt.figure()
 df.plot(linewidth=0.05)
 plt.show()
-
-# ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-# ts = ts.cumsum()
-# ts.plot()
-# plt.show()
-#
-# df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
-# df = df.cumsum()
-# plt.figure()
-# df.plot()
-# # 增加后即可展示图片，否则pycharm不展示图片
-# plt.show()
---
+++ deep/ssq/visual/ssq_shap_data.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# filename可以直接从盘符开始，标明每一级的文件夹直到csv文件，header=None表示头部为空，sep=' '表示数据间使用空格作为分隔符，如果分隔符是逗号，只需换成 ‘，’即可。
+data = pd.read_csv('../ball_items.csv', header=None, sep=',')
+
+rows = data.shape[0]
+colums = data.shape[1]
+print("rows", rows, "colums", colums)
+
+origin = np.matrix(data)
+print(origin)
+print(origin.shape)
+print("origin.dtype: ", origin.dtype)
+
+title = origin[0:1, 0:colums]
+ball_data = origin[1:rows, 0:colums]
+
+date = ball_data[:, 0:1]
+red_ball = ball_data[:, 2:8]
+# 数据类型转换 astype
+red_ball = red_ball.astype(int)
+print("red_ball.dtype: ", red_ball.dtype)
+blue_ball = ball_data[:, 8:9]
+blue_ball = blue_ball.astype(int)
+print(title)
+# print(ball_data)
+print(date)
+print(red_ball)
+print(blue_ball)
+
+show_title = title[:, 2:8]
+# df = pd.DataFrame(red_ball, index=date, columns=(x for x in show_title))
+df = pd.DataFrame(red_ball, index=date, columns=list('123456'))
+plt.figure()
+df.plot(linewidth=0.05)
+plt.show()

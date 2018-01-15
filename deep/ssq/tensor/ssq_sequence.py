4efaca182775fd7c7e1ebeb5d047199279be9291
zixcon <zixcon.fan@outlook.com>
2018-01-15 22:02:25 +0800

    Update ssq_sequence.py

--- deep/ssq/tensor/ssq_sequence.py
+++ deep/ssq/tensor/ssq_sequence.py
@@ -5,4 +5,6 @@ title, date, red_ball, blue_ball = getBall(data)
 visual(red_ball, date)
 visual(blue_ball, date)
 
-
+# 参照：
+https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
+http://blog.csdn.net/qq_36511757/article/details/77895316
